d25530edee2cc005f8baf9c9430573931fcdeb97
zzztttkkk <ztkisalreadytaken@gmail.com>
2024-04-10 00:40:30 +0800

    [🧹 Chore] (/): -

--- tests/test.h
+++ tests/test.h
@@ -2,11 +2,8 @@
 
 #include <gtest/gtest.h>
 
-inline int exec_test(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
 #define MAIN \
-int main(int argc, char** argv) { return exec_test(argc, argv); }
+int main(int argc, char** argv) { \
+    ::testing::InitGoogleTest(&argc, argv); \
+    return RUN_ALL_TESTS(); \
+}
